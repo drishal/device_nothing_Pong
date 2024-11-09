1db2bc568e81dafd05c17881ed9460f52ba96aa6
drishal <drishalballaney@gmail.com>
2024-11-09 13:53:56 +0530

    Pong: build aidl usb hal

--- device.mk
+++ device.mk
@@ -521,7 +521,13 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
+    android.hardware.usb-service.qti \
+    android.hardware.usb.gadget-service.qti
+
+PRODUCT_PACKAGES += \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh \
+    usb_compositions.conf
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
