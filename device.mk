b9dce009fb7108d2f2ba221ce8eeeae714323b06
Michael Bestas <mkbestas@lineageos.org>
2024-11-19 15:37:44 +0530

    Pong: switch to aidl thermal hal

--- device.mk
+++ device.mk
@@ -496,7 +496,7 @@ PRODUCT_COPY_FILES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti-v2 \
+    android.hardware.thermal-service.qti \
     android.hardware.thermal@2.0.vendor
 
 # Trusted User Interface
