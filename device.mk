d12a345f7dfcacd21828573e51f807618c0b03fe
drishal <drishalballaney@gmail.com>
2024-12-21 15:53:40 +0530

    Pong: build libprocessgroup.vendor

--- device.mk
+++ device.mk
@@ -230,7 +230,8 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss-V1-ndk_platform.vendor \
     android.hardware.gnss.measurement_corrections@1.1.vendor \
     android.hardware.gnss.visibility_control@1.0.vendor \
-    android.hardware.gnss@2.1.vendor
+    android.hardware.gnss@2.1.vendor \
+    libprocessgroup.vendor \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf
